c5dcb34571820b99fb83eb47fa44fdbefa61b152
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-06-06 14:35:17 +0300

    fixing signup and login bugs

--- flask_web_app/static/scripts/processLogInForm.js
+++ flask_web_app/static/scripts/processLogInForm.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#login-form').on('submit', function(event) {
-        let password = $('#password').val().trim();
-        let email = $('#email').val().trim();
+        let password = $('#login_password').val().trim();
+        let email = $('#login_email').val().trim();
 
         // Send the AJAX request
         event.preventDefault();
@@ -10,8 +10,8 @@ $(document).ready(function() {
             "password": password
         }
         $.ajax({
-            // url: "http://mylearnlobby.me/LogInForm",
-            url: "http://localhost/LogInForm",
+            url: "http://mylearnlobby.me/LogInForm",
+            // url: "http://localhost/LogInForm",
             method: "post",
             data: JSON.stringify(formData),
             contentType: 'application/json',
@@ -19,20 +19,19 @@ $(document).ready(function() {
                 if (jqXHR.status === 200) {
                     // alert(`meassage: ${jqXHR.responseJSON.message}, user_id: ${jqXHR.responseJSON.user_id}`);
                     // Redirect to the profile page
-                    // window.location.href = `http://mylearnlobby.me/Profile/${jqXHR.responseJSON.user_id}`;
-                    window.location.href = `http://localhost/Profile/${jqXHR.responseJSON.user_id}`;
+                    window.location.href = `http://mylearnlobby.me/Profile/${jqXHR.responseJSON.user_id}`;
+                    // window.location.href = `http://localhost/Profile/${jqXHR.responseJSON.user_id}`;
                 } else {
-                    alert(jqXHR.responseJSON.error);
+                    // alert(jqXHR.responseJSON.error);
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: jqXHR.responseJSON.error,
+                        footer: '<a href="#"></a>'
+                    });
                 }
             }
         });
 
-
-        // function add_to_session (user_id) {
-        //     $.get(`http://localhost:5000/manage_sessions/${user_id}`, function(jqXHR, textStatus) {
-        //         alert(`result: ${jqXHR.responseJSON.result}`)
-        //     });
-            
-        // }
     });
 });
