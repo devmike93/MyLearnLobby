82aa53673590a0d954afdaf4a7d05218a6f3be62
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-06-09 00:06:13 +0300

    fetch courses data #1

--- flask_web_app/static/scripts/processFetchCoursesInfo.js
+++ flask_web_app/static/scripts/processFetchCoursesInfo.js
@@ -1,24 +1,31 @@
 $(document).ready(function() {
-    const user_id = $("body").attr('user_id');
-    const url_get_courses_api = `http://mylearnlobby.me/api/v1/${user_id}/courses`
+    user_id = $("body").attr('user_id');
+    url_get_courses_api = `http://mylearnlobby.me/api/v1/${user_id}/courses`
 
-    let coursesList = [];
-    getAllCourses(url_get_courses_api)
-    .then(data => {
-        console.log("Courses List: ", coursesList);
-    })
-    .catch(error => {
-        console.error("Error fetching courses: ", error);
-    });
+    coursesList = []
 
-    async function getAllCourses(url) {
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
+    fetch(url)
+        .then(response => { response.json(); })
+        .then(data => {
             coursesList = data.courses_list;
-        } catch (error) {
+        }).then(() => {
+            console.log("Courses List: ", coursesList);
+        })
+        .catch(error => {
             console.error("Error fetching courses: ", error);
-        }
-        return coursesList;
-    }
+        });
+    
+
+    // function getAllCourses(url) {
+    //     coursesList = [];
+    //     fetch(url)
+    //     .then(response => { response.json(); })
+    //     .then(data => {
+    //         coursesList = data.courses_list;
+    //     })
+    //     .catch(error => {
+    //         console.error("Error fetching courses: ", error);
+    //     });
+    //     return coursesList;
+    // }
 });
