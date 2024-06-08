91f38395dc571ef216970b1a14aaad08125fe93c
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-06-09 01:11:26 +0300

    fetch courses data #5

--- flask_web_app/static/scripts/processFetchCoursesInfo.js
+++ flask_web_app/static/scripts/processFetchCoursesInfo.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     url_get_courses_api = `http://mylearnlobby.me/api/v1/${user_id}/courses`
 
     coursesList = []
+    courseTasksList = []
 
     fetch(url_get_courses_api)
         .then(response => { return response.json(); })
@@ -15,6 +16,37 @@ $(document).ready(function() {
             console.error("Error fetching courses: ", error);
         });
     
+    // Print the data of each course
+
+    for (let course of coursesList) {
+        let course_id = course.id;
+        console.log("Course ID: ", course.id);
+        (async () => {
+            console.log("Course Title: ", course.title);
+            console.log("Course Description: ", course.description);
+            console.log("Course Start Date: ", course.start_date);
+            console.log("Course End Date: ", course.excepted_end_date);
+            console.log("Course Goals: ", course.goals);
+            console.log("Course Resources: ", course.resources);
+
+            try {
+                let url_get_tasks_api = `http://mylearnlobby.me/api/v1/${course_id}/tasks`
+                let response = await fetch(url_get_tasks_api);
+                
+                if (response.status === 200) {
+                    let data = await response.json();
+                    courseTasksList = data;
+                    // alert(data.message);
+                } else {
+                    throw new Error(`Error creating a task: ${response.status}`);
+                }
+            } 
+            catch (error) {
+                console.error("Error", error);
+            }
+        })();
+    }
+
 
     // function getAllCourses(url) {
     //     coursesList = [];
