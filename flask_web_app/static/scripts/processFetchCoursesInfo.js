71400e0e2f76fd9d35d72b75cade68223e76b06c
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-06-09 03:12:37 +0300

    add multible course views #2

--- flask_web_app/static/scripts/processFetchCoursesInfo.js
+++ flask_web_app/static/scripts/processFetchCoursesInfo.js
@@ -49,63 +49,65 @@ $(document).ready(function() {
     }
     // Create a course block
     function createCourseBlock(course, courseTasksList) {
-        let tasksCompleted= 0;
+        let tasksCompleted = 0;
         for (let task of courseTasksList) {
             if (task.done === true) {
                 tasksCompleted += 1;
             }
         }
+        let progressPercentage = (tasksCompleted / courseTasksList.length) * 100;
         let courseDetailsHTML = `
         <div class="course-details">
                 <div class="col">
-                  <h3>
+                <h3>
                     Course title:<span id="courseName" class="course-title"
-                      >${course.title}</span
+                    >${course.title}</span
                     >
-                  </h3>
-                  <div class="row">
+                </h3>
+                <div class="row">
                     <div class="col">
-                      <div class="row field">
+                    <div class="row field">
                         <label>Total tasks:</label>
                         <p>${courseTasksList.length}</p>
-                      </div>
-                      <div class="row field">
+                    </div>
+                    <div class="row field">
                         <label>Tasks Completed:</label>
                         <p>${tasksCompleted}/${courseTasksList.length}</p>
-                      </div>
-                      <div class="row field">
+                    </div>
+                    <div class="row field">
                         <label>Starts at:</label>
                         <p>${course.start_date}</p>
-                      </div>
-                      <div class="row field">
+                    </div>
+                    <div class="row field">
                         <label>Ends at:</label>
                         <p>${course.excepted_end_date}</p>
-                      </div>
+                    </div>
                     </div>
                     <div class="col cours-progress">
-                      <p>Progress:</p>
-                      <div class="progress">
+                    <p>Progress:</p>
+                    <div class="progress">
                         <div
-                          class="progress-bar"
-                          role="progressbar"
-                          style="width: 25%"
-                          aria-valuenow="25"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: ${progressPercentage}%"
+                        aria-valuenow="${progressPercentage}"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
                         >
-                          25%
+                        ${progressPercentage}%
                         </div>
-                      </div>
                     </div>
-                  </div>
+                    </div>
                 </div>
-                <button id="openCourse" class="btn-get-started">
-                  Open Course View
+                </div>
+                <button id="openCourse" course_id=${course.id} class="btn-get-started">
+                Open Course View
                 </button>
-              </div>
+            </div>
         `;
-         // Find the div with the class 'col courses' and add the HTML content
+        // Find the div with the class 'col courses' and append the HTML content
         let coursesDiv = document.querySelector('.col.courses');
-        coursesDiv.innerHTML = courseDetailsHTML;
+        coursesDiv.innerHTML += courseDetailsHTML; // Use '+=' to append
     }
+
 });
